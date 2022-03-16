ede9b54635872720927a77c885e2c60c86f47a7e
AbirhamYazie <abirhamyazie24@gmail.com>
2022-03-16 15:38:03 +0300

    print"_putchar"

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ **/
 int main(void)
 {
-	_putchar("_putchar\n");
+	char abrish[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 7; c++)
+	{
+		_putchar(abrish[c]);
+	}
+	_putchar('\n');
 	return (0);
 }
