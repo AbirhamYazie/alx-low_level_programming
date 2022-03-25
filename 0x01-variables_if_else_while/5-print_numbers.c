9ccbc00d400c4add0f30db5884804c651d233ef1
AbirhamYazie <abirhamyazie24@gmail.com>
2022-03-25 13:23:15 -0700

    print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,14 +5,15 @@
  *
  * Return: 0 success
  */
-int main()
+int main(void)
 {
         int n;
 
         for (n = 0; n <= 9; n++)
         {
-                printf( "%d", n);
+	       	printf( "%d", n);
         }
-        printf("\n");
+       	printf("\n");
+
         return (0);
 }
