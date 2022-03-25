8f1650b6d45b5530736182c063bb7cff9efad3cf
AbirhamYazie <abirhamyazie24@gmail.com>
2022-03-25 11:17:15 -0700

    print lower case alphbet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point .
+ *
+ * return: always 0 (success)
+ */
+int main()
+{
+	int C;
+	for (C = 'a'; C <= 'z'; C++)
+		putchar(C);
+	putchar('\n');
+		return 0;
+}
