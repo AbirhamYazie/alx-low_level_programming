f12930bd210521cb8b3bb2986daace49198f37ce
AbirhamYazie <99495640+AbirhamYazie@users.noreply.github.com>
2022-04-20 14:35:24 +0300

    prints combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * 
+ * main - entry block
+ *
  * Return: 0
- */
-
+ **/
 int main(void)
 {
-    int i = 0;
-    
-    while (i < 10)
-    {
-        putchar(i % 10 + '0');
-        i++;
-        if (i < 10)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('\n');
-    return (0);
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i % 10 + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
