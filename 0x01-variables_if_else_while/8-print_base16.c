b60eec48b056bd5af1a332e338b88ba8137bd7ac
AbirhamYazie <99495640+AbirhamYazie@users.noreply.github.com>
2022-04-20 14:16:38 +0300

    print hexa decimals

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: 0
- */
-
-int main(void)
-{
-      	int i = 0;
-        char c = 'a';
-	while (i < 10)
-        {
-        putchar(i % 10 + '0');
-        i++;
-        }
-        while (c < 'g')
-        {   
-        putchar(c);
-        c++;
-    	}	  
-    
-    	putchar('\n');
-       	return (0);
+#include <stdio.h>                                           
+                                                             
+/**                                                          
+ * main - Entry point                                        
+ *                                                           
+ * Return: 0                                                 
+ */                                                          
+                                                             
+int main(void)                                               
+{                                                            
+        int i = 0;                                           
+        char c = 'a';                                        
+                                                             
+        while(i < 10)                                        
+        {                                                    
+                putchar(i % 10 + '0');                       
+                i++;                                         
+        }                                                    
+        while(c < 'g')                                       
+        {                                                    
+                putchar(c);                                  
+                c++;                                         
+        }                                                    
+        putchar('\n');
+	return (0);        
 }
