a062b7feab1e2d9c5c6a1527e37f20f4c55f9400
AbirhamYazie <abirhamyazie24@gmail.com>
2022-04-20 04:24:07 -0700

    prints hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,22 +5,21 @@
  *
  * Return: 0
  */
-
 int main(void)
 {
       	int i = 0;
         char c = 'a';
+
 	while (i < 10)
         {
-        putchar(i % 10 + '0');
-        i++;
+	        putchar(i % 10 + '0');
+        	i++;
         }
         while (c < 'g')
         {   
-        putchar(c);
-        c++;
+        	putchar(c);
+        	c++;
     	}	  
-    
     	putchar('\n');
        	return (0);
 }
