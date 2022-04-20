53e975edb72ad0616739073db04b11886672dd09
AbirhamYazie <99495640+AbirhamYazie@users.noreply.github.com>
2022-04-20 11:03:28 +0300

    prints base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+    int i = 0;
+    char c = 'a';
+    
+    while (i < 10)
+    {
+        putchar(i % 10 + '0');
+        i++;
+    }
+    
+    while (c < 'g')
+    {
+        putchar(c);
+        c++;
+    }
+    
+    putchar('\n');
+    return (0);
+}
