24869882c3f9959e0ed70afd7e0f0aaaae03988b
AbirhamYazie <abirhamyazie24@gmail.com>
2022-04-20 04:29:38 -0700

    print hexadecimals

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 /**
@@ -23,31 +22,4 @@ int main(void)
     	}	  
     	putchar('\n');
        	return (0);
-=======
-#include <stdio.h>                                           
-                                                             
-/**                                                          
- * main - Entry point                                        
- *                                                           
- * Return: 0                                                 
- */                                                          
-                                                             
-int main(void)                                               
-{                                                            
-        int i = 0;                                           
-        char c = 'a';                                        
-                                                             
-        while(i < 10)                                        
-        {                                                    
-                putchar(i % 10 + '0');                       
-                i++;                                         
-        }                                                    
-        while(c < 'g')                                       
-        {                                                    
-                putchar(c);                                  
-                c++;                                         
-        }                                                    
-        putchar('\n');
-	return (0);        
->>>>>>> b60eec48b056bd5af1a332e338b88ba8137bd7ac
 }
